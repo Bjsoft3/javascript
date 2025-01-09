6c6fc528b7d901fdf0966eef9e4b51b79ea44b5d
bhagirath_rajpurohit <bjsoft3@gmail.com>
2025-01-09 07:44:49 +0530

    String and it's methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,37 @@
+const name = "Bhagirath"
+const repoCount = 30
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Bhagirath')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+const newString = gameName.substring(0, 4);
+// console.log(newString);
+
+const anotherString = gameName.slice(-8, 4);
+// console.log(anotherString);
+
+
+const newStrOne = "     Bhagirath     ";
+// console.log(newStrOne);
+// console.log(newStrOne.trim());
+
+const url = "https://devbj.com/bhagirath%20joshi";
+// console.log(url.replace('%20', '-'));
+
+// console.log(url.includes('joshi'));
+
+
+const splString = "Dev Bj Balera";
+console.log(splString.split(' '));
+
+
+
