2f8d921cbbe8f4261fb7428d2698276eeb66a3e4
bhagirath_rajpurohit <bjsoft3@gmail.com>
2025-01-26 16:07:45 +0530

    Project 4 solution code

--- 07_projects/4-guessTheNumber/script.js
+++ 07_projects/4-guessTheNumber/script.js
@@ -1,71 +1,3 @@
-# Projects related to DOM
-
-## project link
-[Click here](https://stackblitz.com/edit/dom-project-chaiaurcode?file=index.html)
-
-# Solution Code
-
-## Project 1 solution
-
-```Javascript
-const buttons = document.querySelectorAll('.button')
-const body = document.querySelector('body')
-
-buttons.forEach(function(button) {
-    button.addEventListener('click', function(e) {
-        body.style.backgroundColor = e.target.getAttribute('data-color')      
-    })
-})
-```
-
-## Project 2 solution
-
-```javascript
-const form = document.querySelector('form')
-
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    const height = parseInt(document.querySelector('#height').value)
-    const weight = parseInt(document.querySelector('#weight').value)
-    const result = document.querySelector('#result')
-
-    if(height === '' || height < 0 || isNaN(height)) {
-        result.innerHTML = 'Please give a valid height';
-    } else if(weight === '' || weight < 0 || isNaN(weight)) {
-        result.innerHTML = 'Please give a valid weight';
-    } else {
-        const bmi = (weight / ((height * height)/10000)).toFixed(2)
-
-        let rangeTxt;
-        if (bmi > 18.6 && bmi < 24.9) {
-            rangeTxt = 'Normal Range.'
-        } else {
-            rangeTxt = bmi < 18.6 ? 'Under weight' : 'Overwieght'
-        }
-
-        // show the result
-        result.innerHTML = `<span>${bmi}</span><br></span>${rangeTxt}</span>`
-    }
-})
-```
-
-## Project 3 solution code
-
-```javascript
-const clock = document.getElementById('clock')
-
-
-setInterval(() => {
-    let date = new Date()
-    clock.innerHTML = date.toLocaleTimeString();
-}, 1000);
-
-```
-
-## Project 4 solution code
-
-```javascript
 let randomNum = parseInt(Math.random() * 10 + 1);
 
 const submit = document.querySelector("#subt")
@@ -159,5 +91,3 @@ function newGame() {
         playGame = true;
     })
 }
-
-```
